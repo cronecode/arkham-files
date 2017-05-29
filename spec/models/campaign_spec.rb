e950e28a5167b4ffbde741a0f046e818fff91691
Kristina Born <kristinab@nulogy.com>
2017-05-29 14:49:55 -0400

    test coverage for Campaign.create

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "does not exist before creation" do
+    expect(Campaign.count).to eq(0)
+  end
+
+  it "exists after creation" do
+    Campaign.create
+    expect(Campaign.count).to eq(1)
+  end
 end
