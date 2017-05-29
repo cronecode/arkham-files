2947c76685e65557da9e4daac0bb2ce8454ca51b
Kristina Born <kristinab@nulogy.com>
2017-05-29 16:00:09 -0400

    test coverage for welcome message

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
   describe "GET #home" do
+    render_views
+
     it "returns http success" do
       get :home
+
+      expect(response.body).to have_content("Welcome to the Arkham Files")
       expect(response).to have_http_status(:success)
     end
   end
-
 end
