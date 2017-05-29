ae74cceab975ecd1d2bdf3153579587952a329fe
Kristina Born <kristinab@nulogy.com>
2017-05-29 17:33:16 -0400

    test coverage for new campaign form

--- spec/features/add_new_campaign_spec.rb
+++ spec/features/add_new_campaign_spec.rb
@@ -4,7 +4,15 @@ RSpec.describe "Add a new campaign" do
   it "button redirects to campaigns/new" do
     visit root_path
     click_link "Add new campaign"
-    
+
     expect(page).to have_current_path("/campaigns/new")
+  end
+
+  it "creates a new campaign" do
+    visit campaigns_new_path
+    fill_in "Name", :with => "Night of the Zealot"
+    click_button "Submit"
+
+    expect(Campaign.count).to eq(1)
   end
 end
