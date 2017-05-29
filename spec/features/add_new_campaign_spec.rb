f91e1fae7fe732a830ea846c11721b08c3e2e0ed
Kristina Born <kristinab@nulogy.com>
2017-05-29 17:10:52 -0400

    edits spec to reflex button -> link change

--- spec/features/add_new_campaign_spec.rb
+++ spec/features/add_new_campaign_spec.rb
@@ -1,8 +1,10 @@
+require 'rails_helper'
+
 RSpec.describe "Add a new campaign" do
   it "button redirects to campaigns/new" do
     visit root_path
-    click_button "Add new campaign"
-
+    click_link "Add new campaign"
+    
     expect(page).to have_current_path("/campaigns/new")
   end
 end
