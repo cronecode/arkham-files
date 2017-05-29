b39e800dd2d959e4f8eaa3606d5169bfcc13fa11
Kristina Born <kristinab@nulogy.com>
2017-05-29 17:11:44 -0400

    Add New Campaign link redirects to campaigns/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
   get 'campaigns/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  # root 'application#hello'
 end
