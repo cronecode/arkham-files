6bf779b6dd3abf9bf72cfcf527243e6c1eed883c
Kristina Born <kristinab@nulogy.com>
2017-05-29 20:40:39 -0400

    adds Campaigns resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'campaigns/new'
+  # get 'campaigns/new'
+  resource :campaigns
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
