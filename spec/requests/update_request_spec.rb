44fa5090adf9f7b3baecda229168958f1636572b
Kristina Born <kristinab@nulogy.com>
2017-05-31 17:09:37 -0400

    fixes update spec

--- spec/requests/update_request_spec.rb
+++ spec/requests/update_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "/campaign/:id/edit" do
+RSpec.describe "/campaign/:id" do
   describe "#PUT" do
     it "updates the campaign's notes" do
-      campaign = FactoryGirl.create(:campaign)
-      params = { notes: "blah" }
+      @campaign = FactoryGirl.create(:campaign)
+      params = { campaign: { notes: "blah" } }
 
-      put edit_campaign_path(campaign), params: params
+      put campaign_path(@campaign), params: params
 
       expect(response).to be_successful
       expect(response.body.notes).to eq("blah")
