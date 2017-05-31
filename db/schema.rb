565c01f8e3cefe3b933818f224c423ea50a4db3d
Kristina Born <kristinab@nulogy.com>
2017-05-31 17:46:16 -0400

    adds notes to Campaign model and migrates db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529181110) do
+ActiveRecord::Schema.define(version: 20170531211351) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "notes"
   end
 
 end
