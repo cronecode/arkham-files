a1a2271f5355eeb6ef46ad5495313b7176945efb
Kristina Born <kristinab@nulogy.com>
2017-06-01 12:17:24 -0400

    edits top-level describe to match other request specs

--- spec/requests/campaigns_create_spec.rb
+++ spec/requests/campaigns_create_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "POST#create" do
-  it "creates a campaign with a valid name" do
+RSpec.describe "/campaigns" do
+  describe "POST#create" do
+    it "creates a campaign with a valid name" do
 
-    expect do
-      post campaigns_path, params: { campaign:  { name: "Bob's campaign" } }
-    end.to change { Campaign.count }.by(1)
+      expect do
+        post campaigns_path, params: { campaign:  { name: "Bob's campaign" } }
+      end.to change { Campaign.count }.by(1)
 
-    expect(response).to redirect_to edit_campaign_url(Campaign.last)
+      expect(response).to redirect_to edit_campaign_url(Campaign.last)
+    end
   end
 end
