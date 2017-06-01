e97564b0b2434b998bf0295ab48a47ace0e593e6
Kristina Born <kristinab@nulogy.com>
2017-06-01 12:13:22 -0400

    removes knowledge of view contents from StaticPagesController spec

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe "GET #home" do
-    render_views
-
     it "returns http success" do
       get :home
 
-      expect(response.body).to have_content("Welcome to the Arkham Files")
       expect(response).to have_http_status(:success)
     end
   end
