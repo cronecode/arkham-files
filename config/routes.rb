ebe21b747c67e234575f8b7a65f0d0761454f310
Kristina Born <kristinab@nulogy.com>
2017-06-05 11:49:35 -0400

    adds nested Scenarios resource to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  resources :campaigns
+  resources :campaigns do
+    resources :scenarios
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
