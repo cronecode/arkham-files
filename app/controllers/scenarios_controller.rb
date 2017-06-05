9218bbc39c187f4c298c6fdac835539c679c9ebf
Kristina Born <kristinab@nulogy.com>
2017-06-05 13:46:00 -0400

    implements ScenariosController.create

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -3,4 +3,22 @@ class ScenariosController < ApplicationController
     @campaign = Campaign.find params[:campaign_id]
     @scenario = Scenario.new(campaign: @campaign)
   end
+
+  def create
+    @campaign = Campaign.find params[:campaign_id]
+    @scenario = Scenario.new(scenario_params)
+    @campaign.scenarios << @scenario
+    if @scenario.save
+      redirect_to campaign_scenario_path(@campaign, @scenario)
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def scenario_params
+    params.require(:scenario).permit(:name, :campaign_id)
+  end
 end
