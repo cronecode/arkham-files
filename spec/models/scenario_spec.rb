caf113cc5fe7230a8282d05a339badfe9fe3766c
Kristina Born <kristinab@nulogy.com>
2017-06-06 12:38:49 -0400

    red spec for Scenario model validations

--- spec/models/scenario_spec.rb
+++ spec/models/scenario_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Scenario, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  campaign = FactoryGirl.create(:campaign)
+  puts campaign.inspect
+  scenario = FactoryGirl.create(:scenario, name: "Foo", campaign_id: campaign.id)
+  puts scenario.inspect
+
+  it "should be valid" do
+    expect(scenario).to be_valid
+  end
+
+  it "name should be present" do
+    scenario.name = "    "
+    expect(scenario).to_not be_valid
+  end
+
+  it "campaign id should be present" do
+    scenario.campaign_id = nil
+    expect(scenario).to_not be_valid
+  end
 end
