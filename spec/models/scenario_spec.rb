4ad13179da1a76322c909bc6455f9b84168db174
Kristina Born <kristinab@nulogy.com>
2017-06-06 12:58:23 -0400

    instantiate Scenario for each test

--- spec/models/scenario_spec.rb
+++ spec/models/scenario_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Scenario, type: :model do
-  campaign = FactoryGirl.create(:campaign)
-  puts campaign.inspect
-  scenario = FactoryGirl.create(:scenario, name: "Foo", campaign_id: campaign.id)
-  puts scenario.inspect
-
   it "should be valid" do
+    campaign = FactoryGirl.create(:campaign)
+    scenario = FactoryGirl.create(:scenario, name: "Foo", campaign_id: campaign.id)
+
     expect(scenario).to be_valid
   end
 
   it "name should be present" do
+    campaign = FactoryGirl.create(:campaign)
+    scenario = FactoryGirl.create(:scenario, name: "Foo", campaign_id: campaign.id)
+
     scenario.name = "    "
+
     expect(scenario).to_not be_valid
   end
 
   it "campaign id should be present" do
+    campaign = FactoryGirl.create(:campaign)
+    scenario = FactoryGirl.create(:scenario, name: "Foo", campaign_id: campaign.id)
+
     scenario.campaign_id = nil
+
     expect(scenario).to_not be_valid
   end
 end
