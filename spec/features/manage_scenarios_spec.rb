07b23d5716e44fd3f09435fc195dcdcf218cc660
Kristina Born <kristinab@nulogy.com>
2017-06-06 15:48:59 -0400

    red spec for 'add scenario' feature

---
+++ spec/features/manage_scenarios_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Campaign Management", type: :feature do
+  describe "Add a new scenario" do
+    it "creates a scenario" do
+      campaign = FactoryGirl.create(:campaign)
+      visit campaign_path(campaign.id)
+      click_link "Add Scenario"
+
+      within("form") do
+        fill_in "Name", :with => "The Gathering"
+        click_button "Add scenario"
+      end
+
+      expect(page).to have_content("The Gathering")
+    end
+  end
+
+  describe "View a scenario" do
+    it "displays the scenario's information" do
+
+    end
+  end
+
+  describe "Resolve a scenario" do
+    it "records the outcome of a completed scenario" do
+
+    end
+  end
+
+end
+
+
