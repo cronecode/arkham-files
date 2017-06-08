d97698d56546aa87417e5ac48c1fda7fdf9d426c
Kristina Born <kristinab@nulogy.com>
2017-06-08 15:42:30 -0400

    adds resolution enum to Scenario model and runs migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608174934) do
+ActiveRecord::Schema.define(version: 20170608193830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170608174934) do
     t.datetime "updated_at", null: false
     t.string "notes"
     t.integer "victory_display"
+    t.integer "resolution"
   end
 
   add_foreign_key "scenarios", "campaigns", name: "scenarios_campaign_id_fk"
