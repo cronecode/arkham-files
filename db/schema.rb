16a3caf36d0079bb10403297028ccbce96db741f
Kristina Born <kristinab@nulogy.com>
2017-06-08 11:47:21 -0400

    adds notes to Scenario model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602180712) do
+ActiveRecord::Schema.define(version: 20170608154502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170602180712) do
     t.integer "campaign_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "notes"
   end
 
   add_foreign_key "scenarios", "campaigns", name: "scenarios_campaign_id_fk"
