b6034ce849bfd32a556ff8cbfbfc68a645a07d6f
Kristina Born <kristinab@nulogy.com>
2017-06-08 14:50:57 -0400

    validates Scenario#victory_display if it is present

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -4,12 +4,17 @@ class Scenario < ApplicationRecord
   validates :name, presence: true
   validates :campaign_id, presence: true
   validates :notes, length: { minimum: 1 }, if: :notes_present?
-  validates :victory_display, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :victory_display, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, if: :victory_display_present?
+
   before_validation do
     self.notes = self.notes.strip if notes_present?
   end
 
   def notes_present?
     !notes.nil?
+  end
+
+  def victory_display_present?
+    !victory_display.nil?
   end
 end
