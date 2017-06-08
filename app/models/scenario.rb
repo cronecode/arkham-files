95bf628bf13916f235db2e493266ba39eaedaefd
Kristina Born <kristinab@nulogy.com>
2017-06-08 14:00:59 -0400

    validates that victory_display is an integer >= 0

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -4,8 +4,7 @@ class Scenario < ApplicationRecord
   validates :name, presence: true
   validates :campaign_id, presence: true
   validates :notes, length: { minimum: 1 }, if: :notes_present?
-  validates :victory_display, numericality: true
-
+  validates :victory_display, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   before_validation do
     self.notes = self.notes.strip if notes_present?
   end
