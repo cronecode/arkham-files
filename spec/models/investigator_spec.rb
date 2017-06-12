3388655d37f072abcb949338d1994469d0ad4857
Kristina Born <kristinab@nulogy.com>
2017-06-12 14:58:29 -0400

    red model spec for Investigator#mental_trauma

--- spec/models/investigator_spec.rb
+++ spec/models/investigator_spec.rb
@@ -27,4 +27,15 @@ RSpec.describe Investigator, type: :model do
 
     expect(investigator).to be_valid
   end
+
+  it "has mental trauma" do
+    campaign = FactoryGirl.create(:campaign)
+    investigator = Investigator.new(campaign_id: campaign.id,
+                                    name: "Rex Murphy",
+                                    status: "dead",
+                                    physical_trauma: 4,
+                                    mental_trauma: 1)
+
+    expect(investigator).to be_valid
+  end
 end
