8e40e0e13b6b8d196dee90df23846431d84a0fa6
Kristina Born <kristinab@nulogy.com>
2017-06-12 11:45:00 -0400

    red feature spec for adding Investigators

---
+++ spec/features/manage_investigators_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Investigator Management", type: :feature do
+  describe "Add a new investigator" do
+    it "creates a investigator" do
+      campaign = FactoryGirl.create(:campaign)
+      visit campaign_path(campaign)
+
+      expect do
+        within("form") do
+          fill_in "Name", with: "Agnes Baker"
+          click_button "Save"
+        end
+      end.to change { Scenario.count }.by(1)
+    end
+  end
+end
