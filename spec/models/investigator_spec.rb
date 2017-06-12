e13aef1f0def52e07256bf32c558435f25e9f997
Kristina Born <kristinab@nulogy.com>
2017-06-12 15:19:38 -0400

    red model spec for Investigator#unspent_experience

--- spec/models/investigator_spec.rb
+++ spec/models/investigator_spec.rb
@@ -50,4 +50,17 @@ RSpec.describe Investigator, type: :model do
 
     expect(investigator).to be_valid
   end
+
+  it "has unspent experience" do
+    campaign = FactoryGirl.create(:campaign)
+    investigator = Investigator.new(campaign_id: campaign.id,
+                                    name: "Rex Murphy",
+                                    status: "dead",
+                                    physical_trauma: 4,
+                                    mental_trauma: 1,
+                                    experience_earned: 20,
+                                    unspent_experience: 3)
+
+    expect(investigator).to be_valid
+  end
 end
