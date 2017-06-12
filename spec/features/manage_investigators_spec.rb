5fc145a8961d07b1b165fe236d9453116151df1c
Kristina Born <kristinab@nulogy.com>
2017-06-12 14:26:39 -0400

    red spec for 'View Investigator' feature

--- spec/features/manage_investigators_spec.rb
+++ spec/features/manage_investigators_spec.rb
@@ -15,4 +15,27 @@ RSpec.describe "Investigator Management", type: :feature do
       expect(page).to have_content("Agnes Baker")
     end
   end
+
+  describe "View investigator stats" do
+    it "displays an investigator's information" do
+      campaign = FactoryGirl.create(:campaign)
+      investigator = Investigator.new(name: "Skids",
+                                      campaign_id: campaign.id,
+                                      status: "Insane",
+                                      physical_trauma: 2,
+                                      mental_trauma: 6,
+                                      experience_earned: 10,
+                                      unspent_experience: 5)
+      
+      visit campaign_path(campaign)
+      click_link "Skids"
+
+      expect(page).to have_field("Status", value: "Insane")
+      expect(page).to have_field("Physical Trauma", value: 2)
+      expect(page).to have_field("Mental Trauma", value: 6)
+      expect(page).to have_field("Experience Earned", value: 10)
+      expect(page).to have_field("Unspent Experience", value: 5)
+
+    end
+  end
 end
