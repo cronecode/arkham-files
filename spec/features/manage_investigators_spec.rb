f9a1d155bfd22e2b094c4dec905f5b21290e1d85
Kristina Born <kristinab@nulogy.com>
2017-06-12 12:24:36 -0400

    correct feature expectation

--- spec/features/manage_investigators_spec.rb
+++ spec/features/manage_investigators_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe "Investigator Management", type: :feature do
       visit campaign_path(campaign)
       click_link "Add Investigator"
 
-      expect do
-        within("form") do
-          fill_in "Name", with: "Agnes Baker"
-          click_button "Save"
-        end
-      end.to change { Scenario.count }.by(1)
+      within("form") do
+        fill_in "Name", with: "Agnes Baker"
+        click_button "Save"
+      end
+
+      expect(page).to have_content("Agnes Baker")
     end
   end
 end
