72032e70b473a7106eb6701b16207693a11342f1
Kristina Born <kristinab@nulogy.com>
2017-06-12 12:25:16 -0400

    implements InvestigatorsController.create

--- app/controllers/investigators_controller.rb
+++ app/controllers/investigators_controller.rb
@@ -3,4 +3,20 @@ class InvestigatorsController < ApplicationController
     @campaign = Campaign.find(params[:campaign_id])
     @investigator = Investigator.new
   end
+
+  def create
+    @campaign = Campaign.find(params[:campaign_id])
+    @investigator = Investigator.new(investigator_params.merge(campaign: @campaign))
+    if @investigator.save
+      redirect_to campaign_investigator_path(@campaign, @investigator)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def investigator_params
+    params.require(:investigator).permit(:name)
+  end
 end
