a0c4defb0c56d828856e4db2925833e13e1325d2
Kristina Born <kristinab@nulogy.com>
2017-06-14 13:48:40 -0400

    adds default name to Investigator factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :investigator do
+    name "Zoey Samaras"
   end
   factory :scenario do
     name "The House Always Wins"
     complete false
     order 1
   end
-  
   factory :campaign do
     name "Dunwich Legacy"
   end
