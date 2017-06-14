b10f6608ba4cc70105a96af33c2e5caae7fba37e
Kristina Born <kristinab@nulogy.com>
2017-06-14 15:03:36 -0400

    sets Investigator defaults after init

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -3,16 +3,18 @@ class Investigator < ApplicationRecord
 
   enum status: [:ACTIVE, :KILLED, :INSANE]
 
+  after_initialize :set_defaults
+
   validates :name, presence: true, uniqueness: true
-  after_create :set_defaults
+  validates :status, presence: true
 
   private
 
   def set_defaults
-    self.status = "ACTIVE"
-    self.physical_trauma = 0
-    self.mental_trauma = 0
-    self.experience_earned = 0
-    self.unspent_experience = 0
+    self.status ||= "ACTIVE"
+    self.physical_trauma ||= 0
+    self.mental_trauma ||= 0
+    self.experience_earned ||= 0
+    self.unspent_experience ||= 0
   end
 end
