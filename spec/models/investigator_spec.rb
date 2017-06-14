03edcea26ebc33004a3150a09fd49a92f194818a
Kristina Born <kristinab@nulogy.com>
2017-06-14 19:11:33 -0400

    adds default campaign to Investigator factory

--- spec/models/investigator_spec.rb
+++ spec/models/investigator_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Investigator, type: :model do
   it "should be valid" do
-    campaign = FactoryGirl.create(:campaign)
-    investigator = FactoryGirl.create(:investigator, campaign_id: campaign.id)
+    investigator = FactoryGirl.create(:investigator)
 
     expect(investigator).to be_valid
   end
 
   it "name should be present" do
-    campaign = FactoryGirl.create(:campaign)
-    investigator = FactoryGirl.create(:investigator, campaign_id: campaign.id)
+    investigator = FactoryGirl.create(:investigator)
 
     investigator.name = "      "
 
@@ -18,9 +16,8 @@ RSpec.describe Investigator, type: :model do
   end
 
   it "name should be unique" do
-    campaign = FactoryGirl.create(:campaign)
-    investigator_1 = FactoryGirl.create(:investigator, campaign_id: campaign.id, name: "Rex")
-    investigator_2 = FactoryGirl.create(:investigator, campaign_id: campaign.id, name: "Daisy")
+    investigator_1 = FactoryGirl.create(:investigator, name: "Rex")
+    investigator_2 = FactoryGirl.create(:investigator, name: "Daisy")
 
     investigator_2.name = "Rex"
 
@@ -28,8 +25,7 @@ RSpec.describe Investigator, type: :model do
   end
 
   it "is active by default" do
-    campaign = FactoryGirl.create(:campaign)
-    investigator = FactoryGirl.create(:investigator, campaign_id: campaign.id, name: "Rex Murphy")
+    investigator = FactoryGirl.create(:investigator, name: "Rex Murphy")
 
     expect(investigator.status).to eq("ACTIVE")
   end
