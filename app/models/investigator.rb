1d8ca020e60dabc50ea18452d91eb8b2b664be54
Kristina Born <kristinab@nulogy.com>
2017-06-14 11:16:18 -0400

    Investigator is active by default

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -2,4 +2,12 @@ class Investigator < ApplicationRecord
   belongs_to :campaign
 
   enum status: [:ACTIVE, :KILLED, :INSANE]
+
+  after_create :set_defaults
+
+  private
+
+  def set_defaults
+    self.status = "ACTIVE"
+  end
 end
