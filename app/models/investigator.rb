86623bd313b12803f8bd4ce8f476282d9f9c23ad
Kristina Born <kristinab@nulogy.com>
2017-06-14 17:25:16 -0400

    don't set default values on the model

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -3,18 +3,6 @@ class Investigator < ApplicationRecord
 
   enum status: [:ACTIVE, :KILLED, :INSANE]
 
-  after_initialize :set_defaults
-
   validates :name, presence: true, uniqueness: true
   validates :status, presence: true
-
-  private
-
-  def set_defaults
-    self.status ||= "ACTIVE"
-    self.physical_trauma ||= 0
-    self.mental_trauma ||= 0
-    self.experience_earned ||= 0
-    self.unspent_experience ||= 0
-  end
 end
