0ffd0b39df502c0cf78ab984d22db6ac530422e0
Kristina Born <kristinab@nulogy.com>
2017-06-14 11:15:38 -0400

    red spec for default status

--- spec/models/investigator_spec.rb
+++ spec/models/investigator_spec.rb
@@ -18,6 +18,13 @@ RSpec.describe Investigator, type: :model do
     expect(investigator).to be_valid
   end
 
+  it "is active by default" do
+    campaign = FactoryGirl.create(:campaign)
+    investigator = FactoryGirl.create(:investigator, campaign_id: campaign.id, name: "Rex Murphy")
+
+    expect(investigator.status).to eq("ACTIVE")
+  end
+
   it "has physical trauma" do
     campaign = FactoryGirl.create(:campaign)
     investigator = Investigator.new(campaign_id: campaign.id,
