78960dea4989e9cc778fa83eb6fc316aa0e46080
Kristina Born <kristinab@nulogy.com>
2017-06-15 07:53:49 -0400

    adds non-negative validation for Investigator#mental_trauma

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -5,4 +5,5 @@ class Investigator < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :physical_trauma, numericality: { greater_than_or_equal_to: 0 }
+  validates :mental_trauma, numericality: { greater_than_or_equal_to: 0 }
 end
