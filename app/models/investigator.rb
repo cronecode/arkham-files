8d996a656d5e0eebd19cb3f565b65ef8ded8a7fd
Kristina Born <kristinab@nulogy.com>
2017-06-15 08:09:28 -0400

    non-negative validation for Investigator#unspent_experience

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -7,4 +7,5 @@ class Investigator < ApplicationRecord
   validates :physical_trauma, numericality: { greater_than_or_equal_to: 0 }
   validates :mental_trauma, numericality: { greater_than_or_equal_to: 0 }
   validates :experience_earned, numericality: { greater_than_or_equal_to: 0 }
+  validates :unspent_experience, numericality: { greater_than_or_equal_to: 0 }
 end
