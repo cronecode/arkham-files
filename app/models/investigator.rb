6ff7babec1a3ee94494cbb95db9d86106d63345d
Kristina Born <kristinab@nulogy.com>
2017-06-15 07:55:53 -0400

    adds non-negative validation for Investigator#experience_earned

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -6,4 +6,5 @@ class Investigator < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :physical_trauma, numericality: { greater_than_or_equal_to: 0 }
   validates :mental_trauma, numericality: { greater_than_or_equal_to: 0 }
+  validates :experience_earned, numericality: { greater_than_or_equal_to: 0 }
 end
