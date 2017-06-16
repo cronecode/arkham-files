b7306bab0afc74ee52bba9d933d801c960d95fff
Kristina Born <kristinab@nulogy.com>
2017-06-16 15:22:13 -0400

    validates that Campaign#cycle is in the CYCLES hash

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,5 +2,11 @@ class Campaign < ApplicationRecord
   has_many :scenarios, dependent: :destroy
   has_many :investigators, dependent: :destroy
 
+  CYCLES = {
+      NIGHT_OF_THE_ZEALOT: 'Night of the Zealot',
+      DUNWICH_LEGACY: 'The Dunwich Legacy'
+  }
+
   validates :name, presence: true
+  validates :cycle, inclusion: { in: CYCLES.values }
 end
