3a9ea9851453f4e75c466a11341f36eb24f2cf80
Kristina Born <kristinab@nulogy.com>
2017-06-27 14:36:23 -0400

    updates import statements

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Campaigns from './Campaigns'
 import CampaignIndexContainer from './components/CampaignIndexContainer'
-import SpendXPForm from './PurchaseCardsForm'
-import './index.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import './styles/index.css'
 
 var history = createBrowserHistory()
 
