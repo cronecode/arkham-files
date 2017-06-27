b1714153ebf113f9fa3b78e8c8ada74b6a8ee18d
Kristina Born <kristinab@nulogy.com>
2017-06-26 20:17:02 -0400

    use react-router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Campaigns from './Campaigns'
+import CampaignIndexContainer from './components/CampaignIndexContainer'
+import SpendXPForm from './PurchaseCardsForm'
 import './index.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
+var history = createBrowserHistory()
+
+ReactDOM.render(
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/api/campaigns" component={CampaignIndexContainer} />
+    </Switch>
+  </Router>
+  , document.getElementById('root'))
