7fb746df794b021c0a49a7ab0aee99a76a7024fe
Kristina Born <kristinab@nulogy.com>
2017-06-27 15:25:39 -0400

    convert CampaignIndex to class

--- client/src/components/CampaignIndex.js
+++ client/src/components/CampaignIndex.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import CampaignRow from './CampaignRow'
+import React, {Component} from 'react'
+import IndexRow from './IndexRow'
 
-const CampaignIndex = (props) => {
-  var list = []
-  props.campaigns.forEach((campaign) => {
-    list.push(<CampaignRow name={campaign.name} key={campaign.id} />)
-  })
-  return (
-    <div>
-      {list}
-    </div>
-  )
+class CampaignIndex extends Component {
+  render() {
+    var list = []
+    this.props.campaigns.forEach((campaign) => {
+      list.push(<IndexRow name={campaign.name} id={campaign.id} key={campaign.id} />)
+    })
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-6">
+            <div className="thumbnail space-above space-below">
+              <ul>
+                {list}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default CampaignIndex
