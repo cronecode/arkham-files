f9ae20d326504921f624bbdd2e2bffe1d7cad567
Kristina Born <kristinab@nulogy.com>
2017-06-26 20:50:06 -0400

    converts instance vars to local vars

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -1,10 +1,10 @@
 module Api
   class CampaignsController < ApiController
     def index
-      @campaigns = Campaign.all
+      campaigns = Campaign.all
 
       respond_to do |format|
-        format.json { render json: @campaigns }
+        format.json { render json: campaigns }
       end
     end
   end
