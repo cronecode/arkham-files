faa0c7c2cee4dcc79ead790e6f03c29399081fd2
Kristina Born <kristinab@nulogy.com>
2017-06-27 17:50:07 -0400

    adds delete button for IndexRow

--- client/src/components/IndexRow.js
+++ client/src/components/IndexRow.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import DeleteButton from './DeleteButton'
 
 const IndexRow = (props) => {
   return (
@@ -8,6 +9,7 @@ const IndexRow = (props) => {
         <Link className='space-below' to={'/api/campaigns/' + props.id}>{props.name}</Link>
       </div>
       <div className="col-xs-3">
+        <DeleteButton id={props.id} />
       </div>
     </div>
   )
