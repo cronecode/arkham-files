1b76d30d0ed1faab7abf7f2dccef553335370660
Kristina Born <kristinab@nulogy.com>
2017-06-28 08:06:24 -0400

    use HomePage instead of App for root route

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import CampaignIndexContainer from './components/CampaignIndexContainer'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import HomePage from './components/HomePage'
 import './styles/index.css'
 import 'font-awesome/css/font-awesome.css'
 
@@ -12,7 +12,7 @@ var history = createBrowserHistory()
 ReactDOM.render(
   <Router history={history}>
     <Switch>
-      <Route exact path="/" component={App} />
+      <Route exact path="/" component={HomePage} />
       <Route path="/api/campaigns" component={CampaignIndexContainer} />
     </Switch>
   </Router>
